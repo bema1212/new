8915805cb520c68d0e8acf1db4f9e0f946ba0862
bema1212 <bouwmanenergie@outlook.com>
2024-10-17 09:39:50 +0200

    Update get.js

--- api/get.js
+++ api/get.js
@@ -19,31 +19,42 @@ export default async function handler(req, res) {
     const apiUrl2 = `https://opendata.polygonentool.nl/wfs?service=wfs&version=2.0.0&request=getfeature&typename=se:OGC_Warmtevlak,se:OGC_Elektriciteitnetbeheerdervlak,se:OGC_Gasnetbeheerdervlak,se:OGC_Telecomvlak,se:OGC_Waternetbeheerdervlak,se:OGC_Rioleringsvlakken&propertyname=name,disciplineCode&outputformat=application/json&srsname=EPSG:28992&bbox=${target2}`;
 
     const [response0, response2] = await Promise.all([
-      fetch(apiUrl0, { headers: { 'Content-Type': 'application/json' } }),
-      fetch(apiUrl2, { headers: { 'Content-Type': 'application/json' } })
+      fetch(apiUrl0, { headers: { 'Content-Type': 'application/json' } }), // BAG-info obv zoek-adres
+      fetch(apiUrl2, { headers: { 'Content-Type': 'application/json' } }) // Netbeheerders
     ]);
 
     if (response0.ok && response2.ok) {
       const data0 = await response0.json();
       const data2 = await response2.json();
 
+      // Kadastrale info zoek-adres
       const apiUrl3 = `https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&QUERY_LAYERS=Perceelvlak&layers=Perceelvlak&INFO_FORMAT=application/json&FEATURE_COUNT=1&I=2&J=2&CRS=EPSG:28992&STYLES=&WIDTH=5&HEIGHT=5&BBOX=${target2}`;
       const response3 = await fetch(apiUrl3, {
         headers: { 'Content-Type': 'application/json' },
       });
 
-      if (response3.ok) {
+      const [x, y] = target2.split(',').map(coord => parseFloat(coord));
+
+      // Add function 4 for retrieving BAG data for nearby objects
+      const apiUrl4 = `https://service.pdok.nl/lv/bag/wfs/v2_0?service=WFS&version=2.0.0&request=GetFeature&propertyname=&count=200&outputFormat=json&srsName=EPSG:28992&typeName=bag:verblijfsobject&Filter=<Filter><DWithin><PropertyName>Geometry</PropertyName><gml:Point><gml:coordinates>${x},${y}</gml:coordinates></gml:Point><Distance units='m'>50</Distance></DWithin></Filter>`;
+      const response4 = await fetch(apiUrl4, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response3.ok && response4.ok) {
         const data3 = await response3.json();
+        const data4 = await response4.json();
 
         const combinedData = {
-          data0,
-          data2,
-          data3
+          BAG: data0,
+          NETB: data2,
+          KADAS: data3,
+          NEARBY_BAG: data4 // Re-added function 4 results to combinedData
         };
 
         res.status(200).json(combinedData);
       } else {
-        res.status(500).json({ error: "Error fetching data from the WMS API" });
+        res.status(500).json({ error: "Error fetching data from the WMS or WFS API" });
       }
     } else {
       res.status(500).json({ error: "Error fetching data from one or more APIs" });
